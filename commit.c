0f5fd46d3fad061261c38df6deee74e7fd0c8b55
YVLNair <sarangnair69420@gmail.com>
2026-04-15 08:15:14 +0000

    phase 4 step 4

--- commit.c
+++ commit.c
@@ -13,7 +13,7 @@
 //
 // PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
-//phase 4 step 3
+//phase 4 step 4
 
 #include "commit.h"
 #include "index.h"
